eda79996b232ccd083450b60e31c11e94dfd6f10
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2018-02-10 22:46:25 +0100

    Added reducer to help store the sorting for todo's in the active list

---
+++ src/reducers/activeSortedTodosReducer.js
@@ -0,0 +1,5 @@
+export type ActiveSortedTodosReducerState = Array<number>;
+
+export function activeSortedTodosReducer(currentState: ActiveSortedTodosReducerState = [], action: Action) : ActiveSortedTodosReducerState {
+    return currentState;
+}
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -4,11 +4,13 @@ import { combineReducers } from 'redux';
 import { todosReducer } from './todosReducer';
 import { projectsReducer } from './projectsReducer';
 import { currentReducer } from './currentReducer';
+import { activeSortedTodosReducer } from './activeSortedTodosReducer';
 
 var reducers : Object = {
     todos: todosReducer,
     projects: projectsReducer,
-    current: currentReducer
+    current: currentReducer,
+    activeSortedTodos: activeSortedTodosReducer
 }
 
 var rootReducer : Function = combineReducers(reducers);
