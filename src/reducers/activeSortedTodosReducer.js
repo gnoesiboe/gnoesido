c1fcd288e7095698f0aab3ab0c82d9f2004ea104
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2018-02-11 06:38:20 +0100

    Handle move action in active todo list

--- src/reducers/activeSortedTodosReducer.js
+++ src/reducers/activeSortedTodosReducer.js
@@ -3,14 +3,17 @@
 import {
     ADD_TODO,
     UPDATE_TODO,
-    DELETE_TODO
+    DELETE_TODO,
+    MOVE_TODO
 } from '../action/types';
 import type {
     Action,
     AddTodoAction,
     UpdateTodoAction,
-    DeleteTodoAction
+    DeleteTodoAction,
+    MoveTodoAction
 } from '../action/types';
+import { arrayMove } from 'react-sortable-hoc';
 
 export type ActiveSortedTodosReducerState = Array<string>;
 
@@ -49,6 +52,14 @@ function _handleDeleteTodoAction(currentState: ActiveSortedTodosReducerState, ac
     return currentState.filter((todoId: string) => todoId !== action.id);
 }
 
+function _handleMoveTodoAction(currentState: ActiveSortedTodosReducerState, action: MoveTodoAction): ActiveSortedTodosReducerState {
+    if (action.active === false) {
+        return currentState;
+    }
+
+    return arrayMove(currentState, action.oldIndex, action.newIndex);
+}
+
 export function activeSortedTodosReducer(currentState: ActiveSortedTodosReducerState = [], action: Action) : ActiveSortedTodosReducerState {
     switch (action.type) {
         case ADD_TODO:
@@ -65,6 +76,11 @@ export function activeSortedTodosReducer(currentState: ActiveSortedTodosReducerS
             // $ExpectError
             var deleteTodoAction : DeleteTodoAction = (action: Action);
             return _handleDeleteTodoAction(currentState, deleteTodoAction);
+
+        case MOVE_TODO:
+            // $ExpectError
+            var moveTodoAction : MoveTodoAction = (action: Action);
+            return _handleMoveTodoAction(currentState, moveTodoAction);
 
         default:
             return currentState;
