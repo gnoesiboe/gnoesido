0ec118945632ba938de67e50e19eb8affa8f7a86
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2018-02-12 13:08:38 +0100

    Added HR

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends React.Component<Props> {
                             />
                         </div>
                     </div>
+                    <hr />
                     <ProjectList />
                 </div>
             </Provider>
