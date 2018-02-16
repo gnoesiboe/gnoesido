46d4573503f63594313edb2dea4d8b1b63231dc5
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2018-02-16 07:24:25 +0100

    Color calendar of late todo list items, red

--- src/components/todoList/components/todoListItem/components/TodolistItemMeta.js
+++ src/components/todoList/components/todoListItem/components/TodolistItemMeta.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import { formatDateRelativeToToday } from '../../../../../helper/dateTimeHelper';
 import type { Todo } from '../../../../../model/type/Todo';
+import { createToday, createMomentFromDate } from '../../../../../helper/dateTimeHelper';
+import createClassName from 'classnames';
 
 
 type Props = {
@@ -11,18 +13,32 @@ type Props = {
 
 export default class TodolistItemMeta extends React.Component<Props> {
 
-    render() : ?React$Element<any> {
+    _renderStartsAt() : ?React$Element<any> {
         var { todo } = this.props;
 
-        var startsAt = todo.startsAt;
+        var startsAt = createMomentFromDate(todo.startsAt);
 
         if (!startsAt) {
             return null;
         }
 
+        var today = createToday();
+
+        var className = createClassName('todo-list-item-meta-start', {
+            'todo-list-item-meta-start--late': startsAt.isBefore(today, 'day')
+        });
+
+        return (
+            <li className={ className }>
+                <i className="glyphicon glyphicon-calendar" /> { formatDateRelativeToToday(startsAt) }
+            </li>
+        )
+    }
+
+    render() : ?React$Element<any> {
         return (
             <ul className="list-inline todo-list-item-meta-list">
-                <li><i className="glyphicon glyphicon-calendar" /> { formatDateRelativeToToday(startsAt) }</li>
+                { this._renderStartsAt() }
             </ul>
         )
     }
