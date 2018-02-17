c3d42882d7022b7dde556df3f4a787534f2205ee
Gijs Nieuwenhuis <gijs.nieuwenhuis@freshheads.com>
2018-02-17 07:37:04 +0100

    Readability improvement

--- src/middleware/localPersistingMiddleware.js
+++ src/middleware/localPersistingMiddleware.js
@@ -9,12 +9,16 @@ type Store = {
     getState: () => GlobalStateType
 };
 
+function _extractIdFromCurrentUrl() : string {
+    return window.location.pathname.substr(1);
+}
+
 export default (store: Store) => (next: Next) => (action: Action) : any => {
     var response = next(action),
         newState : GlobalStateType = store.getState();
 
     // @todo get better solution than this
-    var id = window.location.pathname.substr(1);
+    var id = _extractIdFromCurrentUrl();
 
     localStorageRepository.save(id, newState);
 
